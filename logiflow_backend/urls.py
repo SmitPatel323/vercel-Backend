a20aa3c6484372a60213798176ff67f32c572929
SmitPatel323 <smitpatel0405@gmail.com>
2025-08-25 23:15:20 +0530

    updated urls.py

--- logiflow_backend/urls.py
+++ logiflow_backend/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", RedirectView.as_view(url="/admin/")),  # redirect root to /admin
+    # path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
 #Tells Django how to serve your images
